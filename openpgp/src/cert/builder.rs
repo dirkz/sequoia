5aec6cc75d539839953eed8a2a9b7f3089c90d38
Justus Winter <justus@sequoia-pgp.org>
2024-09-10 22:56:05 +0200

    openpgp: Rework the secret key material encryption interface.
      - When encrypting secret key material using AEAD, we protect the
        public key material by feeding it as additional data into the AEAD
        mode.  To that end, we need a reference to the public key material.

--- openpgp/src/cert/builder.rs
+++ openpgp/src/cert/builder.rs
@@ -1529,7 +1529,9 @@ impl CertBuilder<'_> {
             Packet::SecretKey({
                 let mut primary = primary.clone();
                 if let Some(ref password) = self.password {
-                    primary.secret_mut().encrypt_in_place(password)?;
+                    let (k, mut secret) = primary.take_secret();
+                    secret.encrypt_in_place(&k, password)?;
+                    primary = k.add_secret(secret).0;
                 }
                 primary
             }),
@@ -1651,7 +1653,9 @@ impl CertBuilder<'_> {
             let signature = subkey.bind(&mut signer, &cert, builder)?;
 
             if let Some(ref password) = self.password {
-                subkey.secret_mut().encrypt_in_place(password)?;
+                let (k, mut secret) = subkey.take_secret();
+                secret.encrypt_in_place(&k, password)?;
+                subkey = k.add_secret(secret).0;
             }
             acc.push(subkey.into());
             acc.push(signature.into());
